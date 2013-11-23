249e17bab906500a0efaf9deb7757f7bf36a4754
Tim Bickerton <tbickerton@thinkthroughmath.com>
2013-11-23 15:43:56 -0500

    Hooks up pics model to connect to form in db

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,5 +4,22 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		# Call Pic.create(:lesson =>1, ....)
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+	private 
+
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :learned, :emotion)
+
+	end
 
 end
