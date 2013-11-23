26872121eed49f39faac76c369176b9c384339b7
Tim Bickerton <tbickerton@thinkthroughmath.com>
2013-11-23 11:44:13 -0500

    Initial commit, bitches

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirehoseApp::Application.config.secret_key_base = '646ffbd340c081c9887c3ae5beb3a67c422a7b5ff097441adb0e0ba92f5bec58971bc75b79c1e84b1d2b6889cec257ab117d10ed8aca962d502c086b58b6e6c5'
